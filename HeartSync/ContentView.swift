c44f1b58de53f9e088f0d0765999185432cd81a5
Amartya2001-droid <65892119+Amartya2001-droid@users.noreply.github.com>
2024-06-21 10:17:23 -0300

    Initial Commit

---
+++ HeartSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartSync
+//
+//  Created by Amartya Karmakar on 2024-06-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartSync/HeartSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartSyncApp.swift
+//  HeartSync
+//
+//  Created by Amartya Karmakar on 2024-06-21.
+//
+
+import SwiftUI
+
+@main
+struct HeartSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
